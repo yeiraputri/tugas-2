e63ae402ed24c29c15c65b928d4100995d89476b
yeira.putri <yeira.putri@ui.ac.id>
2022-09-13 02:07:38 +0700

    first commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_katalog,
+    'nama': 'Yeira',
+    'id': '2106751726'
+    }
+    return render(request, "katalog.html", context)
+
